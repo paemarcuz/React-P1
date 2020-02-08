eb447c1286c0d1d0a0d80fad536aaac8ff372763
Pae Marcuz <paemarcuz@gmail.com>
2020-02-08 15:12:07 +0800

    Updates

---
+++ precache-manifest.7abf833fd9e7dd9e46dd3748dca32e4d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de4bf7d35d63970539012b7076964b65",
+    "url": "/React-P1/index.html"
+  },
+  {
+    "revision": "190c41214d85c83a9834",
+    "url": "/React-P1/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "1d723363cd4b6cb8d472",
+    "url": "/React-P1/static/css/main.20e72787.chunk.css"
+  },
+  {
+    "revision": "190c41214d85c83a9834",
+    "url": "/React-P1/static/js/2.d2203df8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/React-P1/static/js/2.d2203df8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d723363cd4b6cb8d472",
+    "url": "/React-P1/static/js/main.cb9c1aca.chunk.js"
+  },
+  {
+    "revision": "a246db2f8028eaa3df71",
+    "url": "/React-P1/static/js/runtime-main.ee9dbc46.js"
+  }
+]);
